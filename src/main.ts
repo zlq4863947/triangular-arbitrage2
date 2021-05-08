854a6db2a4a9ca3d0a918e0d0618a93d4b0a312a
zlq4863947 <zlq4863947@gmail.com>
2021-05-08 16:37:06 +0900

    框架构建#1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from "@nestjs/common";
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
