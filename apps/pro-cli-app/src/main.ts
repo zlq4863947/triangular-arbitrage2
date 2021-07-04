5ab188c9e5e58ac6869ace5bfa055dc69353a57b
zlq4863947 <zlq4863947@gmail.com>
2021-07-04 23:56:51 +0900

    monorepo化，代码更新#4-1

--- apps/pro-cli-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-
-import { ProCliAppModule } from './pro-cli-app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ProCliAppModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/pro-cli-app/src/pro-cli-app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ProCliAppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
