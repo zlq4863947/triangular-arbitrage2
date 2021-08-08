d39c4f09e68e8b1d3e98b2a2f78752086ee569b1
zlq4863947 <zlq4863947@gmail.com>
2021-08-08 21:15:29 +0900

    Revert "feat: add typeorm mysql"
    This reverts commit 88a12b17e0bc46739463d3e85eddba8881dec2d2.

--- libs/ta2-libs/src/persistence/common/testing/data/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './master-pair.entity';
-export * from './master-exchange.entity';
-export * from './orderbook.entity';
-export * from './transaction.entity';
-export * from './candlestick.entity';
-export * from './ticker.entity';
